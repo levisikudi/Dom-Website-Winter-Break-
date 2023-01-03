ea8923b3dde6fed2c60b70c193b0e2a7db8f4b6d
Levis Sikudi <levisikudi@gmail.com>
2023-01-02 21:11:56 -0500

    title click functionality

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Yolo");
+
+// SELECT AND SET THE TITLE
+let title = document.querySelector(".title")
+
+// CREATE A changeTitle function that changes the title.textcontent
+const changeTitle = (navItem) =>{
+   title.textContent = navItem.textContent
+}
